4fcc7e47e4c20fb9fdf89855d2751c03baa46309
Nicolas Dambreville <122742254+Nicolas-Dmb@users.noreply.github.com>
2024-03-18 21:37:32 +0100

    Update lancer_app.py

--- lancer_app.py
+++ lancer_app.py
@@ -1,10 +1,8 @@
 import os
 import subprocess
 
-# Obtenir le répertoire actuel
-dossier_actuel = os.getcwd()
 
-# Commande pour lancer Flask
+dossier_actuel = os.getcwd()
 commande_flask = ["flask", "run", "--host=0.0.0.0"]
 
 # Exécution de la commande dans le terminal
